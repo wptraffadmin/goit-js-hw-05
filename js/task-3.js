5e4fe7ebc3d17f55656eeb96e4f60ba15e29f9da
wptraffadmin <wptraffadmin@proton.me>
2024-12-30 15:53:28 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 const sortByDescendingFriendCount = (users) => 
-  users.sort((a, b) => b.friends.length - a.friends.length);
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 console.log(
   sortByDescendingFriendCount([
